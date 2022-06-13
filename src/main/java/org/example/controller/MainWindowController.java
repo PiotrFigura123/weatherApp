e4485b9b5e986e21591e33ac2cb846d625f35653
Piotr Figura <pf>
2022-06-13 20:14:21 +0200

    added gitignore file

--- src/main/java/org/example/controller/MainWindowController.java
+++ src/main/java/org/example/controller/MainWindowController.java
@@ -29,7 +29,7 @@ public class MainWindowController {
     @FXML
     private Label displayActualWeatherLabel;
     @FXML
-    private ImageView setMainActualWeather;
+    private ImageView setMainActualWeather1;
     @FXML
     private Label sunriseWindow1Label;
     @FXML
@@ -78,13 +78,16 @@ public class MainWindowController {
     private ScrollPane anchorPaneInScrollPane;
     @FXML
     private AnchorPane scrollPane1;
+    @FXML
+    private Label windSpeedLabel1;
 
 
+    private String key = new Config().getAPI_KEY();
     @FXML
     void searchFirstCityAction() {
-        String key = new Config().getAPI_KEY();
-        APIFunctionsModel.loadweatherForField(firstCityField, imageView1Label,tempCurrentLabel1,
-                tempMaxLabel1,tempMinLabel1,leftBackgroundPane,setMainActualWeather,sunriseWindow1Label,
+
+        APIFunctionsModel.loadweatherForField(firstCityField, windSpeedLabel1,tempCurrentLabel1,
+                tempMaxLabel1,tempMinLabel1,leftBackgroundPane,setMainActualWeather1,sunriseWindow1Label,
                 sunsetWindow1Label,key);
         APIFunctionsModel.loadFiveDaysWeather(firstCityField,day1Window1Day,day2Window1Day,day3Window1Day,day4Window1Day,day5Window1Day,
                 day1Window1Max,day2Window1Max,day3Window1Max,day4Window1Max,day5Window1Max,
@@ -95,22 +98,87 @@ public class MainWindowController {
     }
 
     @FXML
-    private TextField secondCityField;
+    private Label day1Window1Max2;
 
     @FXML
-    private Label imageView1Label;
+    private Label tempCurrentLabel2;
+    @FXML
+    private Label day1Window1Min2;
+
+    @FXML
+    private ImageView day1Window1Pisture2;
+
+    @FXML
+    private Label day2Window1Max2;
+
+    @FXML
+    private Label day2Window1Min2;
+
+    @FXML
+    private ImageView day2Window1Pisture2;
+
+    @FXML
+    private Label day3Window1Max2;
+
+    @FXML
+    private Label day3Window1Min2;
+
+    @FXML
+    private ImageView day3Window1Pisture2;
+
+    @FXML
+    private Label day4Window1Max2;
+
+    @FXML
+    private Label day4Window1Min2;
+
+    @FXML
+    private ImageView day4Window1Pisture2;
+
+    @FXML
+    private Label day5Window1Max2;
+
+    @FXML
+    private Label day5Window1Min2;
+
+    @FXML
+    private ImageView day5Window1Pisture2;
+
+    @FXML
+    private Label windSpeedLabel2;
 
     @FXML
     private AnchorPane rightBackgroundPane;
+
     @FXML
-    private static ImageView actualWeather1Image;
+    private AnchorPane scrolePane2;
 
-    private APIFunctionsModel apiFunctionsModel;
+    @FXML
+    private TextField secondCityField;
 
+    @FXML
+    private ImageView setMainActualWeather2;
+
+    @FXML
+    private Label tempMaxLabel2;
+
+    @FXML
+    private Label tempMinLabel2;
+
+    @FXML
+    private Label sunriseWindow2Label;
+    @FXML
+    private Label sunsetWindow2Label;
+    @FXML
+    private ScrollPane anchorPaneInScrollPane2;
+    @FXML
+    private AnchorPane scrollPane2;
     @FXML
     void searchSecondCityAction() {
-
-        //APIFunctionsModel.loadweatherForField1(secondCityField.getText());
+        APIFunctionsModel.loadweatherForField(secondCityField, windSpeedLabel2,tempCurrentLabel2,
+                tempMaxLabel2,tempMinLabel2,rightBackgroundPane,setMainActualWeather2,sunriseWindow2Label,
+                sunsetWindow2Label,key);
+        APIFunctionsModel.loadHourlyWeather( anchorPaneInScrollPane2,scrollPane2,key);
 
     }
 
