987a837ec87a5b718377d214fcf59e19e7b366e6
Piotr Figura <pf>
2022-06-21 14:13:20 +0200

    deleted config from repo

--- src/main/java/org/weatherApp/Config.java
+++
@@ -1,8 +0,0 @@
-package org.weatherApp;
-
-public class Config {
-    private static final String API_KEY = "448b701c42f8e23b59da9f63b5194666";
-    public String getAPI_KEY(){
-        return API_KEY;
-    }
-}
